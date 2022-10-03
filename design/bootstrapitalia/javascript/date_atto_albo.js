73cd18b474b146306ce105d01a06841fe013ffb4
Luca Realdi <luca.realdi@opencontent.it>
2022-10-03 13:26:59 +0200

    Fix override bootstrapitalia

---
+++ design/bootstrapitalia/javascript/date_atto_albo.js
@@ -0,0 +1,225 @@
+$(document).ready(function () {
+    moment.locale('it');
+
+    var esecutivita = $('.esecutivita').find('select');
+
+    var tipo_archiviazione = $('.tipo_archiviazione').find('select');
+
+    var isBandoOConcorso = $('.data_fine_validita').length > 0;
+
+    var setEsecutivita = function (value) {
+        var setDateValue = getDate('data_iniziopubblicazione').add(11, 'd');
+        if (value === '1') {
+            setDate('data_esecutivita', getDate('data'));
+            setDate('data_efficacia', getDate('data'));
+        } else if (value === '0') {
+            setDate('data_esecutivita', setDateValue);
+            setDate('data_efficacia', setDateValue);
+        }
+
+        readonlyDate('data_esecutivita');
+        readonlyDate('data_efficacia');
+    };
+
+    var calculateDateForArchiviazione = function () {
+        var dateValue = getDate('data_iniziopubblicazione').add(10, 'd');
+        if (isBandoOConcorso) {
+            dateValue = getDate('data_fine_validita').add(1, 'd');
+        }
+
+        return dateValue;
+    };
+
+    var setArchiviazione = function (value) {
+        setDateValue = calculateDateForArchiviazione();
+        if (value === 'riservato') {
+            setDate('data_finepubblicazione', setDateValue);
+            enableDate('data_finepubblicazione');
+            readonlyDate('data_finepubblicazione');
+
+            setDate('data_archiviazione', null);
+            disableDate('data_archiviazione');
+
+            // closeDateAttributeGroup();
+        } else if (value === 'archiviato') {
+            setDate('data_archiviazione', setDateValue);
+            enableDate('data_archiviazione');
+            readonlyDate('data_archiviazione');
+
+            setDate('data_finepubblicazione', null);
+            disableDate('data_finepubblicazione');
+
+            // closeDateAttributeGroup();
+        } else if (value === 'custom') {
+            //setDate('data_archiviazione', null);
+            enableDate('data_archiviazione');
+            notReadonlyDate('data_archiviazione');
+
+            //setDate('data_finepubblicazione', null);
+            enableDate('data_finepubblicazione');
+            notReadonlyDate('data_finepubblicazione');
+
+            openDateAttributeGroup();
+        }
+    };
+
+    var getDate = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day').val();
+            var month = container.find('.month').val();
+            var year = container.find('.year').val();
+            return moment(day + "-" + month + "-" + year, "DD-MM-YYYY");
+        }
+
+        return moment("Invalid date", "DD-MM-YYYY");
+    };
+
+    var disableDate = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day').attr("type", "hidden");
+            var month = container.find('.month').attr("type", "hidden");
+            var year = container.find('.year').attr("type", "hidden");
+            container.find('.fa-calendar').hide();
+            container.find('span.readonlyDate').remove();
+        }
+    };
+
+    var enableDate = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day').attr("type", "text");
+            var month = container.find('.month').attr("type", "text");
+            var year = container.find('.year').attr("type", "text");
+            container.find('.fa-calendar').show();
+            container.find('span.readonlyDate').remove();
+        }
+    };
+
+    var readonlyDate = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day').attr("type", "hidden");
+            var month = container.find('.month').attr("type", "hidden");
+            var year = container.find('.year').attr("type", "hidden");
+            container.find('.fa-calendar').hide();
+            container.find('span.readonlyDate').remove();
+            container.append('<span class="readonlyDate">' + day.val() + '/' + month.val() + '/' + year.val() + '</span>');
+        }
+    };
+
+    var notReadonlyDate = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day').attr("type", "text");
+            var month = container.find('.month').attr("type", "text");
+            var year = container.find('.year').attr("type", "text");
+            container.find('.fa-calendar').show();
+            container.find('span.readonlyDate').remove();
+        }
+    };
+
+    var setDate = function (identifier, date) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day');
+            var month = container.find('.month');
+            var year = container.find('.year');
+            if (date && date.isValid()) {
+                day.val(date.format('DD'));
+                month.val(date.format('MM'));
+                year.val(date.format('YYYY'));
+            } else {
+                day.removeAttr('value');
+                month.removeAttr('value');
+                year.removeAttr('value');
+            }
+        }
+    };
+
+    var isEmpty = function (identifier) {
+        var container = $('.' + identifier);
+        if (container.length > 0) {
+            var day = container.find('.day');
+            var month = container.find('.month');
+            var year = container.find('.year');
+            return ($.trim(day.val()).length + $.trim(month.val()).length + $.trim(year.val()).length) === 0;
+        }
+    };
+
+    var openDateAttributeGroup = function () {
+        if ($('#accordion-panel-date').css('height') === "0px") {
+            $('h2#accordion-header-date').trigger('click');
+        }
+    };
+    var closeDateAttributeGroup = function () {
+        if ($('#accordion-panel-date').css('height') !== "0px") {
+            $('h2#accordion-header-date').trigger('click');
+        }
+    };
+
+    $('.data input').on('change', function () {
+        setEsecutivita(esecutivita.val());
+    });
+
+    if (isBandoOConcorso) {
+        $('.data_fine_validita input').on('change', function () {
+            setArchiviazione(tipo_archiviazione.val());
+        });
+    }
+
+    esecutivita.on('change', function () {
+        setEsecutivita($(this).val());
+    });
+
+    tipo_archiviazione.on('change', function () {
+        setArchiviazione($(this).val());
+    });
+
+
+    if (isEmpty('data_esecutivita')) {
+        setEsecutivita(esecutivita.val());
+    } else {
+        readonlyDate('data_esecutivita');
+    }
+    if (isEmpty('data_efficacia')) {
+        setEsecutivita(esecutivita.val());
+    } else {
+        readonlyDate('data_efficacia');
+    }
+
+    if (tipo_archiviazione.data('version') > 1) {
+        if (isEmpty('data_archiviazione') && isEmpty('data_finepubblicazione')) {
+            console.log('empty data_archiviazione and data_finepubblicazione: set default');
+            setArchiviazione(tipo_archiviazione.val());
+        } else if (isEmpty('data_archiviazione') && !isEmpty('data_finepubblicazione')
+            && getDate('data_finepubblicazione').format("DD-MM-YYYY") === calculateDateForArchiviazione().format("DD-MM-YYYY")) {
+            console.log('empty data_archiviazione and valid data_finepubblicazione: set tipo_archiviazione riservato');
+            tipo_archiviazione.val('riservato');
+            tipo_archiviazione.trigger('change');
+        } else if (isEmpty('data_finepubblicazione') && !isEmpty('data_archiviazione')
+            && getDate('data_archiviazione').format("DD-MM-YYYY") === calculateDateForArchiviazione().format("DD-MM-YYYY")) {
+            console.log('valid data_archiviazione and empty data_finepubblicazione: set tipo_archiviazione archiviato');
+            tipo_archiviazione.val('archiviato');
+            tipo_archiviazione.trigger('change');
+        } else {
+            if (!isEmpty('data_archiviazione') && getDate('data_archiviazione').format("DD-MM-YYYY") !== calculateDateForArchiviazione().format("DD-MM-YYYY")) {
+                console.log('custom data_archiviazione (' + getDate('data_archiviazione').format("DD-MM-YYYY") + ' != ' + calculateDateForArchiviazione().format("DD-MM-YYYY") + '): set custom tipo_archiviazione');
+            }
+            if (!isEmpty('data_finepubblicazione') && getDate('data_finepubblicazione').format("DD-MM-YYYY") !== calculateDateForArchiviazione().format("DD-MM-YYYY")) {
+                console.log('custom data_finepubblicazione (' + getDate('data_finepubblicazione').format("DD-MM-YYYY") + ' != ' + calculateDateForArchiviazione().format("DD-MM-YYYY") + '): set custom tipo_archiviazione');
+            }
+            tipo_archiviazione.val('custom').trigger('change');
+            openDateAttributeGroup();
+        }
+    } else if (isEmpty('data_archiviazione') && isEmpty('data_finepubblicazione')) {
+        setArchiviazione(tipo_archiviazione.val());
+    }
+
+    if (isEmpty('data_finepubblicazione_trasparenza')) {
+        var anno_iniziopubblicazione = getDate('data_iniziopubblicazione').year();
+        setDate('data_finepubblicazione_trasparenza', moment("31-12-" + (anno_iniziopubblicazione + 5), "DD-MM-YYYY"));
+    }
+
+});
--- design/bootstrapitalia/override/templates/datatype/edit/multifile_first_version.tpl
+++ design/bootstrapitalia/override/templates/datatype/edit/multifile_first_version.tpl
@@ -1,52 +1,44 @@
+{def $disallow_upload = cond(and($attribute.version|gt(1), $attribute.content|count()), true(), false())}
 {default attribute_base=ContentObjectAttribute}
     <div id="uploader_{$attribute_base}_data_multibinaryfilename_{$attribute.id}">
 
+        <div class="clearfix upload-file-list" data-sorturl="{concat('ocmultibinary/sort/', $attribute.id, '/', $attribute.version, '/', $attribute.language_code  )|ezurl(no)}">
+        {include uri="design:content/datatype/view/filelist.tpl" attribute=$attribute}
+        </div>
+
         {def $file_count = 0}
-
-        {if $attribute.has_content}
-            <table class="list" cellpadding="0" cellspacing="0">
-                <tr>
-                    <th>
-                        File allegati:
-                        <button class="btn btn-default btn-xs pull-right" type="submit"
-                                name="CustomActionButton[{$attribute.id}_delete_binary]" title="Rimuovi tutti i file">
-                            Elimina tutti i file
-                        </button>
-                    </th>
-                </tr>
-                {foreach $attribute.content as $file}
-                    <tr>
-                        <td>
-                            <button class="ocmultibutton btn btn-link btn-xs" type="submit"
-                                    name="CustomActionButton[{$attribute.id}_delete_multibinary][{$file.filename}]"
-                                    title="Rimuovi questo file"><img src="{'trash.png'|ezimage(no)}"/></button>
-                            {$file.original_filename|wash( xhtml )}&nbsp;({$file.filesize|si( byte )})
-                        </td>
-                    </tr>
-                {/foreach}
-            </table>
-        {else}
-            <p>Nessun file caricato.</p>
-        {/if}
-
         {if $attribute.has_content}
             {set $file_count = $attribute.content|count()}
         {/if}
-        {if or($file_count|lt( $attribute.contentclass_attribute.data_int2 ), $attribute.contentclass_attribute.data_int2|eq(0) )}
-            <div class="block">
-                <label class="ocmultilabel"
-                       for="ezcoa-{if ne( $attribute_base, 'ContentObjectAttribute' )}{$attribute_base}-{/if}{$attribute.contentclassattribute_id}_{$attribute.contentclass_attribute_identifier}">{'New file for upload'|i18n( 'design/standard/content/datatype' )}
-                    :</label>
-                <input type="hidden" name="MAX_FILE_SIZE" value="{$attribute.contentclass_attribute.data_int1}000000"/>
-                <input id="ezcoa-{if ne( $attribute_base, 'ContentObjectAttribute' )}{$attribute_base}-{/if}{$attribute.contentclassattribute_id}_{$attribute.contentclass_attribute_identifier}"
-                       class="box ezcc-{$attribute.object.content_class.identifier} ezcca-{$attribute.object.content_class.identifier}_{$attribute.contentclass_attribute_identifier}"
-                       name="{$attribute_base}_data_multibinaryfilename_{$attribute.id}" type="file"/>
-                <input class="ocmultibutton btn btn-default btn-sm" type="submit"
-                       name="CustomActionButton[{$attribute.id}_upload_multibinary]" value="Allega file"
-                       title="Allega il file"/>
+        {if and($disallow_upload|not(), or($file_count|lt( $attribute.contentclass_attribute.data_int2 ), $attribute.contentclass_attribute.data_int2|eq(0) ))}
+            <div class="clearfix upload-button-container">
+                <span class="btn btn-success btn-sm fileinput-button">
+                    <i class="fa fa-plus"></i>
+                    <span>{'Add file'|i18n( 'extension/ocmultibinary' )}</span>
+                    <input class="input-upload" multiple type="file" name="OcMultibinaryFiles[]"
+                           data-url="{concat('ocmultibinary/upload/', $attribute.id, '/', $attribute.version, '/', $attribute.language_code  )|ezurl(no)}" />
+
+
+                </span>
+            </div>
+            <div class="clearfix upload-button-spinner" style="display: none">
+                <a class="btn btn-success btn-sm" href="#"><i class="fa a fa-circle-o-notch fa-spin"></i> {'Add file'|i18n( 'extension/ocmultibinary' )}</a>
             </div>
         {/if}
 
 
     </div>
 {/default}
+
+
+{ezscript_require( array( 'ezjsc::jquery', 'ezjsc::jqueryio', 'ezjsc::jqueryUI', 'jquery.fileupload.js','jquery.ocmultibinary.js') )}
+{ezcss_require( 'jquery.fileupload.css' )}
+
+{if $disallow_upload|not()}
+<script>
+    $(document).ready(function(){ldelim}
+        $('#uploader_{$attribute_base}_data_multibinaryfilename_{$attribute.id}').ocmultibinary();
+    {rdelim});
+</script>
+{/if}
+{undef $disallow_upload}
